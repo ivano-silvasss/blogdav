aac001bb0634f6b6ff3f2c216043fd34752e9340
VIADO <ivano.silvasss@gmail.com>
2023-04-09 08:56:45 -0300

    novodosnovo

--- data/headerNavLinks.js
+++
@@ -1,8 +0,0 @@
-const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projetos' },
-  { href: '/about', title: 'Sobre' },
-]
-
-export default headerNavLinks
